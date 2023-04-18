4da84d2d1e27c2ee72f6fbfb64e5f8e0d740dc0e
Qhawe <q@gmail.com>
2023-04-18 02:29:25 -0700

    c macros and preprocessors

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - is used as refernce in this case a data type
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	printf("%s", __FILE__);
+
+	return (0);
+}
