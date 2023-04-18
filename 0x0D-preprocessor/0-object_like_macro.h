a66ff708e38fd82b8d6d68860130e6fb77b8f76b
Qhawe <q@gmail.com>
2023-04-17 19:50:01 -0700

    C macros

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,16 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * macros are include with the #define directive
+ * the header file has been abbreviated with the token
+ * betty will specify it requires Underscore not .h
+ * the name of the file was included in the two direcives
+ * the two directives are used to specify
+ * the ifindef enures that the macro code hs nt been used before
+ */
+
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main will print the name of the file
+ * returns a success
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+
+	return (0);
+}
+#endif
