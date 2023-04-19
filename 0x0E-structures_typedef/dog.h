8a01fde76fd8cd91da2d9f154224d2f64fab0c9b
Qhawe <q@gmail.com>
2023-04-18 18:49:51 -0700

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - gives a a grouped list of the dogs owner info
+ * @name: is the first member
+ * @age: is the second memeber
+ * @owner: is the third member
+ *
+ * Description: longer description
+ * the struct would allow the dfferent variables to be accessed
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
