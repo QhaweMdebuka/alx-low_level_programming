8a9a85e094458fd0406c7952eb8253b43ad66fbe
Qhawe <qhawemdebukag12@gmail.com>
2023-05-09 15:54:48 -0700

    nput and output of fles

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile- Read text file print output
+ * @filename: pinter to the fle to be read
+ * @letters: number of letters to be printed
+ * Return: w- the number of bytes
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
- *
- * Return: If the function fails or filename is NULL - -1.
- *         If the file does not exist the user lacks write permissions - -1.
- *         Otherwise - 1.
+ * append_text_to_file - function that appends the text in the file
+ * @filename: pointer to the filename
+ * @text_content: string used to aapend content into text
+ * Return: 1 if success and -1 if failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
