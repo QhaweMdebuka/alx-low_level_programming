477a928c048f4a3c21d315b3eff9880beac17f42
QhaweMdebuka <qhawemdebukag12@gmail.com>
2024-01-17 03:05:35 -0800

    dynamic libraries

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - scripts the character o to standard output
+ * @c: character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set.
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
+}
